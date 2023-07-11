f163f5d9e92c8b1cf160c577bba57475deb2baa8
roxtape <refizaderamiz97@gmail.com>
2023-07-11 20:13:52 +0300

    asdas

---
+++ script.js
@@ -0,0 +1,10 @@
+
+let aze = +prompt('Deyismek istediginiz meblegi yazin');
+let euro = aze / 1.85 ;
+let usd = aze / 1.7 ;
+let tl = aze / 0.06523 ;
+let rub = aze / 0.01861 ;
+document.getElementById('ramo').innerHTML = aze + ' ₼ = ' + euro.toFixed() + ' € ' ;
+document.getElementById('ss').innerHTML = aze + ' ₼ = ' + usd.toFixed() + ' $  ' ;
+document.getElementById('aa').innerHTML =  aze + ' ₼ = ' + tl.toFixed() +  ' ₺' ;
+document.getElementById('bb').innerHTML = aze + ' ₼ = ' + rub.toFixed() + ' ₽ ' ;
